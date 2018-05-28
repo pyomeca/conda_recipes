b3c1ba5359c21df8fee920c5f57528e45bfb7ed4
Pariterre <pariterre@hotmail.com>
2018-05-28 10:31:02 -0400

    change lines

--- ezc3d/bld.bat
+++ ezc3d/bld.bat
@@ -2,6 +2,8 @@
 mkdir build
 cd build
 
-cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_PYTHON=ON
+cmake ../^
+    -DCMAKE_INSTALL_PREFIX=$PREFIX^
+    -DBUILD_PYTHON=ON
 cmake --build ./ --config Release|x64 --target install
 
