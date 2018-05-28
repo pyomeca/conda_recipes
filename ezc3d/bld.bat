496b18eb66056c8dca1d6a35cdfdb02653f9e13d
Pariterre <pariterre@hotmail.com>
2018-05-28 10:32:19 -0400

    Update bld.bat
    Added compiler name

--- ezc3d/bld.bat
+++ ezc3d/bld.bat
@@ -3,7 +3,8 @@ mkdir build
 cd build
 
 cmake ../^
+    -G"Visual Studio 14 2015 Win64"^
     -DCMAKE_INSTALL_PREFIX=$PREFIX^
     -DBUILD_PYTHON=ON
-cmake --build ./ --config Release|x64 --target install
+cmake --build ./ --config Release --target install
 
