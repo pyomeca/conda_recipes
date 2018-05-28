53bad6a32aee415677c9e42cb7a9a29d47ec413d
Pariterre <pariterre@hotmail.com>
2018-05-28 15:39:21 -0400

    Windows done!

--- ezc3d/bld.bat
+++ ezc3d/bld.bat
@@ -1,10 +1,12 @@
-#!/bin/bash
 mkdir build
 cd build
 
 cmake ../^
     -G"Visual Studio 14 2015 Win64"^
-    -DCMAKE_INSTALL_PREFIX=$PREFIX^
-    -DBUILD_PYTHON=ON
-cmake --build ./ --config Release --target install
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
+    -DBUILD_PYTHON=ON^
+    -DPython3_EXECUTABLE="%PREFIX%/python.exe"
 
+cmake --build ./^
+    --config Release^
+    --target install
