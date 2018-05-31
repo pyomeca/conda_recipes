b4deef275ed31fa07ee959a26d6ae3a0bfc3f4bf
Pariterre <pariterre@hotmail.com>
2018-05-31 17:45:33 -0400

    indentation changes

--- rbdl/bld.bat
+++ rbdl/bld.bat
@@ -4,7 +4,7 @@ cd build
 cmake ../^
     -G"Visual Studio 14 2015 Win64"^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
-    -DRBDL_BUILD_STATIC=OFF
+    -DRBDL_BUILD_STATIC=ON
 
 cmake --build ./^
     --config Release^
