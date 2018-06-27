f8aa3db94b62e93f1a383a3454ece9d4108dbdbf
Pariterre <pariterre@hotmail.com>
2018-06-27 17:37:20 -0400

    added matlab compilation to conda recipe of biorbd

--- biorbd/build.sh
+++ biorbd/build.sh
@@ -5,7 +5,9 @@ cd build
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DC_WRAPPER=ON \
-      -DPYTHON3_WRAPPER=ON
+      -DPYTHON3_WRAPPER=ON \
+      -DMATLAB_WRAPPER=ON \
+      -DMatlab_TOOLBOX_INSTALL_DIR=$PREFIX/MATLAB
 
 make -j $CPU_COUNT
 make install
