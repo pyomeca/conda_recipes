02ebcd4592792fd2cd9b8f435310593e10ec50ee
Pariterre <pariterre@hotmail.com>
2018-08-10 12:02:40 -0400

    Update receipe for ezc3d

--- ezc3d/bld.bat
+++ ezc3d/bld.bat
@@ -5,6 +5,8 @@ cmake ../^
     -G"Visual Studio 14 2015 Win64"^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
     -DBUILD_PYTHON=ON^
+    -DBUILD_MATLAB=ON^
+	-DBUILD_EXAMPLE=OFF^
     -DPython3_EXECUTABLE="%PREFIX%/python.exe"
 
 cmake --build ./^
--- ezc3d/build.sh
+++ ezc3d/build.sh
@@ -4,7 +4,10 @@ cd build
 
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DBUILD_PYTHON=ON
+	  -DBUILD_EXAMPLE=OFF \
+      -DBUILD_PYTHON=ON \
+	  -DBUILD_MATLAB=ON \
+	  -DMatlab_ezc3d_INSTALL_DIR=$PREFIX/MATLAB
 
 make -j $CPU_COUNT
 make install
