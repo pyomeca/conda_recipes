08b2a208be28c44e7a99ab8b79577bfc5d468ab0
Pariterre <pariterre@hotmail.com>
2018-10-19 11:58:28 -0400

    New build for ezc3d

--- ezc3d/build.sh
+++ ezc3d/build.sh
@@ -3,11 +3,11 @@ mkdir build
 cd build
 
 cmake ../ \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-	  -DBUILD_EXAMPLE=OFF \
-	  -DBINDER_MATLAB=ON \
-	  -DMatlab_ezc3d_INSTALL_DIR=$PREFIX/MATLAB \
-      -DBINDER_PYTHON3=ON \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DBUILD_EXAMPLE=OFF \
+    -DBINDER_MATLAB=ON \
+    -DMatlab_ezc3d_INSTALL_DIR=$PREFIX/MATLAB \
+    -DBINDER_PYTHON3=ON
 
 make -j $CPU_COUNT
 make install
